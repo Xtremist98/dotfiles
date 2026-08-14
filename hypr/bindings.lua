b74dec25f315f149802a91484a55db8eaadd0bb5
Xtremist98 <sohailandriod25800@gmail.com>
2026-08-14 15:49:27 +0500

    Automated backup

--- hypr/bindings.lua
+++ hypr/bindings.lua
@@ -37,8 +37,9 @@ hl.bind("SUPER + M", hl.dsp.exec_cmd("uwsm app -- /usr/bin/mpv --player-operatio
 hl.bind("SUPER + D", hl.dsp.exec_cmd("uwsm app -- /usr/lib/vesktop/vesktop"), { description = "Discord" })
 hl.bind("SUPER + Y", hl.dsp.exec_cmd("omarchy-launch-tui nvtop"), { description = "Nvtop" })
 hl.bind("SUPER + Q", hl.dsp.exec_cmd("uwsm app -- /usr/bin/qbittorrent"), { description = "Torrent" })
-hl.bind("SUPER + Z", hl.dsp.exec_cmd("env QT_STYLE_OVERRIDE=kvantum dolphin"), { description = "Dolphin" })
+hl.bind("SUPER + Z", hl.dsp.exec_cmd("dolphin"), { description = "Dolphin" })
 o.bind("SHIFT + TAB","Workspace overview","omarchy-shell shell summon mirador '{}'")
+hl.bind("SUPER + SHIFT + T", hl.dsp.exec_cmd("uwsm app -- /usr/bin/Telegram"), { description = "Telegram" })
 
 -- Brightness control with arrow keys
 hl.bind("SUPER + Up", hl.dsp.exec_cmd("brightnessctl set +1%"))
