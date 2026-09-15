dfc7a248a131f2772c2e266e37cd921da6de111f
Xtremist98 <sohailandriod25800@gmail.com>
2026-09-15 13:13:20 +0500

    Automated backup

--- omarchy-themes/everpuccin/hyprland.lua
+++ omarchy-themes/everpuccin/hyprland.lua
@@ -4,26 +4,11 @@ local inactive_border_color = "rgba(232A2Eaa)"
 
 hl.config({
   general = {
-    gaps_in = 1,
-    gaps_out = 1,
     col = {
       active_border = active_border_color,
       inactive_border = inactive_border_color,
     },
   },
-
-  decoration = {
-    rounding = 0,
-    active_opacity = 1.0,
-    inactive_opacity = 1.0,
-    shadow = {
-      enabled = false,
-    },
-    blur = {
-      enabled = false,
-    },
-  },
-
   group = {
     col = {
       border_active = active_border_color,
